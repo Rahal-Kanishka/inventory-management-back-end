6ccfea033808105214f64c3271f9203aa293cbd7
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-09 15:06:08 -0500

    recipe API endpoints added

--- routes/receipyRoute.py
+++ routes/receipyRoute.py
@@ -26,6 +26,24 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@router.get("/recipes")
+@router.get("/recipe/all")
 async def get_all_recipes(db: db_dependency):
     return db.query(models.models.Recipe).all()
+
+
+@router.put("/recipe/edit")
+async def updateRecipe(db: db_dependency):
+    return db.query(models.models.Recipe).all()
+
+
+@router.post("/recipe/add")
+async def create_recipe_(db: db_dependency, recipe_data: BaseRecipeCreate):
+    ingredient_list = []
+    if recipe_data:
+        db_recipe = models.models.Recipe(**recipe_data.dict())
+        db_recipe.created_by = 1
+        ingredient_list.append(db_recipe)
+        db.add_all(ingredient_list)
+        db.commit()
+        db.refresh(db_recipe)
+        return db_recipe
