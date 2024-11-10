96e7e24760a51109a2c33f90d044806ab6570ae5
Ben S <bensparks96@gmail.com>
2024-11-10 12:44:19 -0500

    Testing Git

--- utils/database.py
+++ utils/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# DB_URL = 'mysql+pymysql://root:root1234@host.docker.internal:3306/intelicheff'
+# DB_URL = 'mysql+pymysql://root:!QAZ2wsx@host.docker.internal:3306/intelicheff'
 DB_URL = config('DB_URL')
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
