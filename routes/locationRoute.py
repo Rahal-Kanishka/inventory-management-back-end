b210fdced348bde9d2507288d8e688acc1e5107f
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-17 23:02:07 -0500

    add end point to get locations based on assign user id

--- routes/locationRoute.py
+++ routes/locationRoute.py
@@ -41,6 +41,16 @@ async def get_location_by_id(id: int, db: db_dependency):
     return location
 
 
+@router.get("/location/user/{user_id}", summary="filter assigned Locations based on given user")
+async def get_locations_by_user(db: db_dependency, user_id: int):
+    user = db.query(models.Location).filter(models.Location.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User not found!")
+
+    # Get users assigned to location
+    return db.query(models.Location).join(models.Location.users).filter(models.User.id == user_id).all()
+
+
 @router.get("/location/users/all", summary='Get all locations with assigned users')
 async def get_all_locations_with_users(db: db_dependency):
     return db.query(models.Location).options(joinedload(models.Location.users)).all()
@@ -96,7 +106,10 @@ async def remove_user_from_location(db: db_dependency, user_id: int, location_id
     db.add(location)
     db.commit()
 
-    return await get_locations_with_assigned_users(db, user_id)
+    return (db.query(models.User)
+            .join(models.User.locations)  # Join the locations relationship
+            .filter(models.Location.id == location_id)  # Eager load locations if needed
+            .all())
 
 
 @router.put("/location/assign_user/{user_id}/{location_id}", summary="Assign location to a user")
@@ -113,11 +126,14 @@ async def assign_user_to_location(db: db_dependency, user_id: int, location_id: 
     location.users.append(user)
     db.add(location)
     db.commit()
-    return await get_locations_with_assigned_users(db, user_id)
+    return (db.query(models.User)
+            .join(models.User.locations)  # Join the locations relationship
+            .filter(models.Location.id == location_id)  # Eager load locations if needed
+            .all())
 
 
-async def get_locations_with_assigned_users(db, location_id: int):
+async def get_location_with_assigned_users(db, location_id: int):
     return (db.query(models.Location)
             .options(joinedload(models.Location.users))
             .filter(models.Location.id == location_id)  # Join the locations relationship
-            .all())
+            .first())
