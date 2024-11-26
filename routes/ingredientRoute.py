62f6827f3befffe6dfb79e01a734b70bbe20a36f
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-26 16:18:18 -0500

    ingredient related changes and removal of product type table

--- routes/ingredientRoute.py
+++ routes/ingredientRoute.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, HTTPException, Depends, File, UploadFile, Form
 from fastapi.params import Query
+from sqlalchemy import Integer
+from sqlalchemy.sql.functions import coalesce
 from starlette import status
 
 import models.models
 from classes.classes import BaseRecipe, BaseIngredient, BaseRecipeCreate, UpdateBaseIngredient
 from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.sql import func, cast
 from typing import Annotated, List
 
 from utils.database import SessionLocal
@@ -26,7 +29,24 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.get("/ingredient/all")
 async def getAllIngredient(db: db_dependency):
-    return db.query(models.models.Ingredient).all()
+    result = (db
+              .query(models.models.Ingredient.id,
+                     models.models.Ingredient.name,
+                     models.models.Ingredient.description,
+                     cast(func.sum(coalesce(models.models.GRN_has_Ingredient.currentQuantity, 0)), Integer).label(
+                         'totalQuantity'))
+              .join(models.models.GRN_has_Ingredient,
+                    models.models.Ingredient.id == models.models.GRN_has_Ingredient.Ingredient_id,
+                    isouter=True)
+              .group_by(models.models.Ingredient.id, models.models.Ingredient.name,
+                        models.models.Ingredient.description)
+              .all())
+    # convert to dictionary type
+    processed_results = [
+        {"id": ingredient_id, "name": name, "description": description, "totalQuantity": int(totalQuantity)}
+        for ingredient_id, name, description, totalQuantity in result
+    ]
+    return processed_results
 
 
 @router.put("/ingredient/edit")
@@ -39,22 +59,65 @@ async def updateIngredient(db: db_dependency, updateBaseIngredient: UpdateBaseIn
     # Update the item fields
     db_item.id = updateBaseIngredient.id
     db_item.name = updateBaseIngredient.name
-    db_item.currentQuantity = updateBaseIngredient.currentQuantity
     db_item.description = updateBaseIngredient.description
-    db_item.image = updateBaseIngredient.image
 
     # Commit the changes to the database
     db.commit()
     db.refresh(db_item)  # Optional: refresh instance with database values
+    print('record updated: ', db_item)
 
-    return db_item
+    result = (db
+              .query(models.models.Ingredient.id,
+                     models.models.Ingredient.name,
+                     models.models.Ingredient.description,
+                     cast(func.sum(models.models.GRN_has_Ingredient.currentQuantity), Integer).label('totalQuantity'))
+              .join(models.models.GRN_has_Ingredient,
+                    models.models.Ingredient.id == models.models.GRN_has_Ingredient.Ingredient_id)
+              .group_by(models.models.Ingredient.id, models.models.Ingredient.name,
+                        models.models.Ingredient.description)
+              .filter(models.models.Ingredient.id == db_item.id)
+              .first())
+
+    # convert to dictionary type
+    if result:
+        result_dict = {
+            "id": result[0],
+            "name": result[1],
+            "description": result[2],
+            "totalQuantity": result[3],
+        }
+        print(result_dict)
+        return result_dict
 
 
 @router.post("/ingredient/add")
 async def createIngredient(db: db_dependency, ingredient: BaseIngredient):
     db_ingredient = models.models.Ingredient(**ingredient.dict())
-    db_ingredient.currentQuantity = 0
     db.add(db_ingredient)
     db.commit()
     db.refresh(db_ingredient)
-    return db_ingredient
+    print('item created: ', db_ingredient.id)
+    result = (db
+              .query(models.models.Ingredient.id,
+                     models.models.Ingredient.name,
+                     models.models.Ingredient.description,
+                     cast(func.sum(coalesce(models.models.GRN_has_Ingredient.currentQuantity, 0)), Integer).label(
+                         'totalQuantity'))
+              .join(models.models.GRN_has_Ingredient,
+                    models.models.Ingredient.id == models.models.GRN_has_Ingredient.Ingredient_id,
+                    isouter=True)
+              .group_by(models.models.Ingredient.id, models.models.Ingredient.name,
+                        models.models.Ingredient.description)
+              .filter(models.models.Ingredient.id == db_ingredient.id)
+              .first())
+
+    # convert to dictionary type
+    if result:
+        result_dict = {
+            "id": result[0],
+            "name": result[1],
+            "description": result[2],
+            "totalQuantity": result[3] if result[3] is not None else 0,
+        }
+        print(result_dict)
+        return result_dict
